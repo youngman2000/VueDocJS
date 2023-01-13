14dbe0a39ca483544bd214622dd393236725dd60
antname <1406422800@qq.com>
2023-01-13 19:43:24 +0800

    update

--- src/main.js
+++ src/main.js
@@ -13,10 +13,13 @@ import router from "./router";
 import "./assets/main.css";
 // highlightjs
 import "highlight.js/styles/atom-one-dark.css";
-import "highlight.js/lib/common"; // side effect import will not be optimized out by webpack
+import hljsCommon from "highlight.js/lib/common"; // side effect import will not be optimized out by webpack
 import hljsVuePlugin from "@highlightjs/vue-plugin";
 
 import precode from "./components/precode.vue";
+
+// 注意：解决Vue使用highlight.js build打包发布后样式消失问题，原因是webpack在打包的时候没有把未被使用的代码打包进去，因此，在此处引用一下，看似无意义实则有用
+hljsCommon.highlightAuto('<h1>Highlight.js has been registered successfully!</h1>').value
 
 const app = createApp(App);
 
